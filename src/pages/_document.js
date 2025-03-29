2db9d30ace49eb17362318108a09696945b268d4
Yankwokho <paulyan8@gmail.com>
2025-03-29 17:18:11 -0400

    Create _document.js

---
+++ src/pages/_document.js
@@ -0,0 +1,19 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        {/* Main content */}
+        <Main />
+
+        {/* Example: Add chatbot script before </body> */}
+        <script src="//code.tidio.co/vtlpzoq5sheunpd6apmapzumgdsunayk.js" async></script>
+
+        {/* Next.js scripts */}
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
